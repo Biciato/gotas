1fcbdc053df0a9a4d050c52f087ab64bb45739de
Gustavo Souza Gonçalves <gustavosouzagoncalves@outlook.com>
2018-08-10 02:12:22 -0300

    config.inc.php

--- webroot/phpMyAdmin/config.inc.php
+++ webroot/phpMyAdmin/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = '2a$07$7WiVrxfw9eVbscmFzSoZLeRc6mFKXYZucH6Db3ce06Zkx4Y4El0MW'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'sIlM9sGq\JB,vv:E8C/1R{r5l0L80gZc'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 // $cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
@@ -30,7 +30,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+// $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
