ddb8694e7e2f9e9ba059bff2e49e22673014c86c
Gustavo Souza Gonçalves <gustavosouzagoncalves@hotmail.com>
2019-03-10 23:51:52 -0300

    r

--- config/definitions.full.php
+++ config/definitions.full.php
@@ -8,16 +8,17 @@
  * @since 2019-02-19
  */
 
+// host
+const __HOST__ = "sistema-devel.gotas.local";
+// Server
+// const __HOST__ = "sistema.gotas.com.br";
+
 // Branch Master -> Servidor RTI
-const __SERVER__ = "https://sistema.gotas.com.br/";
+// const __SERVER__ = "https://sistema.gotas.com.br/";
 // Branch Devel -> Servidor RTI
 // const __SERVER__ = "https://sistema-devel.gotas.com.br/";
 // Branch Master-> Servidor local
 // const __SERVER__ = "https://sistema.gotas.local/";
 // Branch Devel -> Servidor local
 // const __SERVER__ = "https://sistema-devel.gotas.local/";
-
-// host
-const __HOST__ = "sistema.gotas.local";
-// Server
-// const __HOST__ = "sistema.gotas.com.br";
+const __SERVER__ = "https://" . __HOST__ . "/";
