8b0520ab05913b365df11810ae2cd0e852d78cd9
Gustavo Souza Gonçalves <gustavosouzagoncalves@hotmail.com>
2019-04-16 11:04:34 -0300

    [UPDATE] #CORS Apple

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -251,6 +251,8 @@ class AppController extends Controller
         $this->response->header('Access-Control-Allow-Headers', 'X-Requested-With');
         $this->response->header('Access-Control-Allow-Headers', 'Content-Type, x-xsrf-token');
         $this->response->header('Access-Control-Max-Age', '172800');
+        $this->response->header('Access-Control-Allow-Origin', '*');
+        $this->response->header('Access-Control-Allow-Credentials', true);
 
         $this->_initializeUtils();
 
