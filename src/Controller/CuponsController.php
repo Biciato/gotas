6f3b38cb2d329899331b84c5ac2cbb19d615e791
Gustavo Souza Gonçalves <gustavosouzagoncalves@hotmail.com>
2019-09-06 19:23:31 -0300

    [UPDATE] Ajustes

--- src/Controller/CuponsController.php
+++ src/Controller/CuponsController.php
@@ -2291,7 +2291,7 @@ class CuponsController extends AppController
                 $tipoPerfil = $usuarioLogado->tipo_perfil;
                 $funcionario["nome"] = $usuarioLogado->nome;
                 $isFuncionario = false;
-                $turnos = $this->ClientesHasQuadroHorario->getHorariosCliente(null, $cliente->id);
+                $turnos = $this->ClientesHasQuadroHorario->getHorariosCliente(null, $cliente["id"]);
                 $turnos = $turnos->toArray();
                 $turnoAtual = ShiftUtil::obtemTurnoAtual($turnos);
 
@@ -2357,7 +2357,7 @@ class CuponsController extends AppController
                 $cuponsPendentes = array();
 
                 foreach ($cupons as $cupom) {
-                    if (($cupom->clientes_id != $cliente->id) || !$cupom->brinde->brinde_rede) {
+                    if (($cupom->clientes_id != $cliente["id"]) || !$cupom->brinde->brinde_rede) {
                         // Impede resgate de brinde se o brinde não for do mesmo posto ou se o brinde não for de rede
                         $errors = [MSG_CUPONS_ANOTHER_STATION];
                         $errorCodes = [MSG_CUPONS_ANOTHER_STATION_CODE];
@@ -2501,7 +2501,7 @@ class CuponsController extends AppController
 
                             $transacao = new CuponsTransacoes();
                             $transacao->redes_id = $rede->id;
-                            $transacao->clientes_id = $cliente->id;
+                            $transacao->clientes_id = $cliente["id"];
                             $transacao->cupons_id = $cupom->id;
                             $transacao->brindes_id = $cupom->brindes_id;
                             $transacao->clientes_has_quadro_horario_id = $turnoAtual["id"];
